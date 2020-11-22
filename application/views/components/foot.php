9d52666a688dda4b78f3f20722a9593863af0020
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2020-11-23 00:04:16 +0500

    Modified the footer file, added another column into the footer menu for account, signin and signup

--- application/views/components/foot.php
+++ application/views/components/foot.php
@@ -2,18 +2,38 @@
 <script src="<?= base_url('assets/js/jquery.min.js'); ?>"></script>
 <script src="<?= base_url('assets/js/bootstrap.min.js'); ?>"></script>
 
-<div class="jumbotron bg-secondary mb-0">
+<div class="jumbotron bg-dark mb-0">
     <div class="container">
         <div class="row text-white">
-            <div class="col-xl-lg-4 col-md-4 col-sm-12">
-                <h2><a class="text-white" href="<?= base_url(); ?>">Hotels</a></h2>
+            <div class="col-xl-lg-3 col-md-3 col-sm-12">
+                <h4><a class="text-white" href="<?= base_url(); ?>">Hotels</a></h4><br>
                 <p>Hotels is a site developed for putting all hotels' data on a webiste to tourists who visit Swat.</p>
             </div>
-            <div class="col-xl-lg-4 col-md-4 col-sm-12">
-                col 2
+            <div class="col-xl-lg-3 col-md-3 col-sm-12">
+                <h4>Quick Links</h4>
+                <p>
+                    <a href="<?= base_url(); ?>" class="text-white">Home</a><br>
+                    <a href="<?= base_url('about'); ?>" class="text-white">About</a><br>
+                    <a href="<?= base_url('hotels'); ?>" class="text-white">Hotels</a><br>
+                    <a href="<?= base_url('locations'); ?>" class="text-white">Locations</a>
+                </p>
+                
             </div>
-            <div class="col-xl-lg-4 col-md-4 col-sm-12">
-                col 3
+            <div class="col-xl-lg-3 col-md-3 col-sm-12">
+                <h4>Follow Us</h4>
+                <p>
+                    <a href="#" class="text-white">Facebook</a><br>
+                    <a href="#" class="text-white">Twitter</a><br>
+                    <a href="#" class="text-white">Instagram</a><br>
+                    <a href="#" class="text-white">LinkedIn</a>
+                </p>
+            </div>
+            <div class="col-xl-lg-3 col-md-3 col-sm-12">
+                <h4>Accounts</h4>
+                <p>
+                    <a href="" class="text-white">Sign In &raquo;</a><br>
+                    <a href="" class="text-white">Create Account &raquo;</a>
+                </p>
             </div>
         </div>
     </div>
